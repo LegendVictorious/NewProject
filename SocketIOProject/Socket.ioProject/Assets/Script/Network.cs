e5ff7cf48823aec73cf84dd323365c1e992bf793
Patten <ejpatten@labs.neit.edu>
2018-02-15 12:21:49 -0500

    Folder hierarchy corrected

---
+++ SocketIOProject/Socket.ioProject/Assets/Script/Network.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+    static SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+	}
+	
+	void OnConnected (SocketIOEvent e) {
+        Debug.Log("We are connected");
+	}
+}
--- app.js
+++
@@ -1,7 +0,0 @@
-var io =  require('socket.io')(process.envPort||3000);
-
-console.log("Server started");
-
-io.on('connection', function(socket){
-    console.log('client connected');
-});
