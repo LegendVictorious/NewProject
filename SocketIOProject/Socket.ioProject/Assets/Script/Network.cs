80614687d36b3b6cc51fb687ac4a8d154d65c603
Patten <ejpatten@labs.neit.edu>
2018-02-28 12:50:14 -0500

    Networked movement, rotation and animation

---
+++ SocketIOProject/Socket.ioProject/Assets/Script/Network.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+    static SocketIOComponent socket;
+    public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
+
+	// Use this for initialization
+	void Start ()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("spawn player", OnSpawn);
+        socket.On("disconnected", OnDisconnected);
+        players = new Dictionary<string, GameObject>();
+	}
+	
+	void OnConnected (SocketIOEvent e)
+    {
+        Debug.Log("We are connected");
+        socket.Emit("playerhere");
+	}
+
+    void OnSpawn(SocketIOEvent e)
+    {
+        string id = e.data["id"].ToString();
+        Debug.Log("Player spawned: " + id);
+        GameObject player = Instantiate(playerPrefab);
+        players.Add(id, player);
+        Debug.Log("Count: " + players.Count);
+    }
+
+    void OnDisconnected(SocketIOEvent e)
+    {
+        string id = e.data["id"].ToString();
+        Debug.Log("Player disconnected: " + id);
+        GameObject player = players[id];
+        Destroy(player);
+        players.Remove(id);
+    }
+}
